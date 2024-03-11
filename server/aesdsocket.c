210d7eb72762ae6d7802d98678bd36bbdf45bdc6
Jihoon Kim <jihoonkim413@gmail.com>
2024-03-11 10:03:49 -0700

    aesdsocket.c

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,97 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <syslog.h>
-#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <pthread.h>
-#include <sys/queue.h>
-#include <sys/time.h>
-#define PORT 9000
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-int main(int argc, char const* argv[])
+#define PORT "9000"  // the port users will be connecting to
+#define DATA_FILE "/var/tmp/aesdsocketdata"
+#define BACKLOG 10   // how many pending connections queue will hold
+
+void sigchld_handler(int s)
 {
-    int status;
-    int sockfd;
-    struct addrinfo hints;
-    struct addrinfo *servinfo;
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void send_data_to_client(int client_socket) {
+    FILE *fp = fopen(DATA_FILE, "r");
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        send(client_socket, buffer, strlen(buffer), 0);
+    }
+    fclose(fp);
+}
+
+int main(void)
+{
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; 
-    
-    if (status = getaddrinfo(NULL, "9000",&hints, &servinfo) !=0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(1);
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
     }
 
-    struct addrinfo *p;
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        // Creating socket file descriptor
-        // freeaddrinfo(servinfo);
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-            perror("socket failed");
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
             exit(1);
         }
-    
-        // Forcefully attaching socket to the port 9000
-        int opt = 1;
-        if (setsockopt(sockfd, SOL_SOCKET,SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-            perror("failed to set socket options");
-            exit(1);
-        }    
 
-        // Forcefully attaching socket to the port 9000
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            perror("bind failed");
-            exit(1);
+            close(sockfd);
+            perror("server: bind");
+            continue;
         }
-
 
         break;
     }
-    freeaddrinfo(servinfo);
 
-    if (listen(sockfd, 10) == -1) {
-        perror("listen");
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
         exit(1);
-    }    
-
-    while (true) {
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
-        int clientfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (clientfd == -1 &&
-            errno != EINTR) {
-            close(sockfd);
-            perror("accept");
-            exit(1);
-        }
-
-        ssize_t valread;
-        char buffer[1024] = { 0 };
-        valread = read(clientfd, buffer,1024 - 1); // subtract 1 for the null
-                                // terminator at the end
-        printf("%s\n", buffer);
-        size_t file_size = sizeof(buffer);
-        send(clientfd, buffer, file_size,0);
-
     }
 
-    // ssize_t valread;
-    // char buffer[1024] = { 0 };
-    // valread = read(clientfd, buffer,1024 - 1); // subtract 1 for the null
-    //                           // terminator at the end
-    // printf("%s\n", buffer);
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
 
-    // freeaddrinfo(servinfo); //free the linked-list
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    // printf("server: waiting for connections...\n");
+    char buffer[1024];
+    ssize_t bytes_received;
+    FILE *fp = fopen(DATA_FILE, "a+");
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+
+        inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr),s, sizeof s);
+        ssize_t bytes_received;
+        FILE *fp = fopen(DATA_FILE, "a+");        
+        printf("server: got connection from %s\n", s);
+        
+        while ((bytes_received = recv(new_fd, buffer, sizeof(buffer), 0)) > 0) {
+            buffer[bytes_received] = '\0';
+            fprintf(fp, "%s", buffer);
+
+            char *newline = strchr(buffer, '\n');
+            if (newline != NULL) {
+                fflush(fp);
+                send_data_to_client(new_fd);
+                memset(buffer, 0, sizeof(buffer));
+            }
+        }
+
+        close(new_fd);  // parent doesn't need this
+    }
 
     return 0;
 }
